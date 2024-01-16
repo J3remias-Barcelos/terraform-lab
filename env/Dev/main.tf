22967c523697b588a164ad33c26cfb8a113e7021
Jeremias Barcelos Carneiro <jbcarneiro@simpress.com.br>
2024-01-16 10:59:18 -0300

    Terraform para focar em Escalabilidade

--- env/Dev/main.tf
+++ env/Dev/main.tf
@@ -3,8 +3,9 @@ module "aws-dev" {
   instancia = "t2.micro"
   regiao_aws = "us-west-2"
   chave = "IaC-DEV"
-}
-
-output "IP" {
-  value = module.aws-dev.IP_publico
+  grupoDeSeguranca = "DEV"
+  minimo = 0
+  maximo = 1
+  nomeGrupo = "DEV"
+  producao = false
 }
--- env/Prod/main.tf
+++ env/Prod/main.tf
@@ -3,8 +3,9 @@ module "aws-prod" {
   instancia = "t2.micro"
   regiao_aws = "us-west-2"
   chave = "IaC-Prod"
-}
-
-output "IP" {
-  value = module.aws-prod.IP_publico
+  grupoDeSeguranca = "Producao"
+  minimo = 1
+  maximo = 10
+  nomeGrupo = "Prod"
+  producao = true
 }
